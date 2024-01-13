bf25b447f0d8eb244f049657f6c5a69b29942de7
David Gallego Gomez <davgalle@c3r14s6.42madrid.com>
2024-01-13 11:19:06 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 12:35:36 by davgalle          #+#    #+#             */
-/*   Updated: 2024/01/13 10:40:53 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/01/13 11:16:45 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ int	ft_check_arg(int argc, char **argv, t_design *design)
 		ft_error_msg("Invalid number of arguments!", NULL);
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
-		ft_error_msg("Map not found!", NULL);
+	{
+		perror("Error opening file!");
+	   	exit(EXIT_FAILURE);
+	}
 	if (ft_strnrcmp(argv[1], ".ber", 4))
 		ft_error_msg("The file is invalid, use a .ber file!", NULL);
 	printf("Listo para crear el mapa \n");
@@ -37,10 +40,7 @@ int	main(int argc, char **argv)
 	t_design	design;
 
 	if (argc == 1)
-	{
-		write (1, "Insert Coin :)", 14);
-		return (1);
-	}
+		ft_error_msg("You have to upload a file to play!", NULL);
 	map = ft_check_arg(argc, argv, &design);
 	printf("mapa creado, listo para iniciar el juego: %i\n", map);
 //	ft_init_game(map, design);
