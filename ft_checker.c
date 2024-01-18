9976f2fd6bc3f3b8f4c2356138689435669289d6
Nicolas Gonzalez de Mendoza <73661812+ErMichoss@users.noreply.github.com>
2024-01-18 12:57:35 +0100

    Add files via upload

---
+++ ft_checker.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_checker.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 20:52:43 by davgalle          #+#    #+#             */
+/*   Updated: 2024/01/18 12:48:44 by nicgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+bool	ft_check_dimension(char *line, t_error *error, size_t file_size)
+{
+	int	i;
+	size_t	len;
+
+	len = ft_strlen_custom(line);
+	printf("len: %zu\n", len);
+	if (len < 7)
+		return (false);
+	if (len != file_size - 1)
+		return (false);
+	i = 0;
+	while (line[i] != '\n' || line[i] != '\0')
+	{
+		if (!is_valid_char(line[i], error))
+			return (false);
+		i++;
+	}
+	printf("Retorna true\n");
+	return (true);
+}
+
+/*bool	ft_check_realmap(char *str, t_design *design)
+{
+	design = ft_new_design();
+	ft_middle_map(str, design);
+	ft_restmap(str, design);
+	return (true);
+}
+
+void	ft_middle_map(char *str, t_design *design)
+{
+	int	i;
+	int	x;
+	int	y;
+	int	z;
+
+	x = 0;
+	y = 0;
+	z = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '1')
+			x++;
+		if (str[i] == '0')
+			y++;
+		if (str[i] == 'E')
+			z++;
+		i++;
+	}
+	design->wall = x;
+	design->space = y;
+	design->exit = z;
+}*/
+
+char	*ft_read_file(int fd, t_error *error, char *str, t_design *design)
+{
+	char	*line;
+	int		i;
+	size_t		file_size;
+
+	line = NULL;
+	design = NULL;
+	file_size = 0;
+	i = 1;
+	while (i > 0)
+	{
+		line = get_next_line(fd);
+		printf("Linea: %s\n", line);
+		if (!line)
+			break;
+		if (file_size == 0)
+			ft_file_size(line, &file_size);
+		if (!ft_check_dimension(line, error, file_size))
+			ft_error_map("Mapa no válido1", line);
+		str = ft_strjoin(str, line);
+		printf("STR despues del JOIN: \n%s\n", str);
+		free(line);
+	}
+	close(fd);
+	/*if (!ft_check_realmap(str, design)) // check de mapa jugable o no.
+		ft_error_map("Mapa no válido", str);*/
+	return (str);
+}
+
+char	**ft_check_map(int fd, t_design *design)
+{
+	char	**map;
+	char	*str;
+	t_error	*error;
+
+	map = NULL;
+	str = NULL;
+	design = NULL;
+	error = ft_new_error();
+	str = malloc(1);
+	if (!str)
+		return (NULL);
+	str[0] = '\0';
+	str = ft_read_file(fd, error, str, design);
+	map = ft_split(str, '\n');
+	free(str);
+	return (map);
+}
---
+++ so_long.h
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 12:37:32 by davgalle          #+#    #+#             */
+/*   Updated: 2024/01/18 12:23:14 by nicgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <mlx.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 7
+# endif
+
+typedef struct	s_error {
+	char	wall;
+	char	space;
+	char	exit;
+	char	player;
+	char	enemys;
+	char	collect;
+
+}				t_error;
+
+typedef struct	s_design {
+	int	wall;
+	int	space;
+	int	exit;
+	int	player;
+	int	enemys;
+	int	collect;
+
+}				t_design;
+
+typedef struct	s_game {
+
+}	t_game;
+
+//*** check map ***
+
+char		**ft_check_arg(int argc, char **argv, t_design *design);
+int			ft_strnrcmp(char *argv, char *str, int count);
+char		**ft_check_map(int fd, t_design *design);
+bool		ft_check_dimension();
+bool		is_valid_char(char c, t_error *error);
+char		*ft_read_file(int fd, t_error *error, char *str, t_design *design);
+bool		ft_check_dimension(char *line, t_error *error, size_t file_size);
+bool		ft_check_realmap(char *line, t_design *design);
+void		ft_middle_map(char *str, t_design *design);
+void		ft_restmap(char *str, t_design *design);
+
+//*** errors ***
+
+void		ft_error_msg(char *msg, char **map);
+void		ft_error_map(char *msg, char *line);
+
+//** create new design node **
+
+t_design	*ft_new_design(void);
+t_error		*ft_new_error(void);
+
+//** GNL **
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(char *str);
+char		*ft_strchr(char *s, int c);
+char		*ft_strjoin(char *board, char *buffer);
+char		*ft_strdup(char *s1);
+
+//** auxiliars **
+
+char		*ft_strdup(char *s1);
+size_t		ft_strlen_custom(char *line);
+void		ft_file_size(char *line, size_t *size);
+
+//** SPLIT **
+
+char		**ft_empty_split(void);
+char		*ft_strdup_custom(const char *s, size_t n);
+char		**ft_free_str(char **aux);
+int			ft_countc(char const *s, char c);
+char		**ft_split(char const *s, char c);
+
+
+#endif
