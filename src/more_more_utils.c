fecc2bb323b6c75c9efe20e8456658a28a49132b
David Gallego Gomez <davgalle@c2r11s3.42madrid.com>
2024-01-25 14:57:42 +0100

    upload

--- src/more_more_utils.c
+++ src/more_more_utils.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:33:24 by davgalle          #+#    #+#             */
-/*   Updated: 2024/01/24 17:38:54 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/01/25 14:55:14 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,35 @@
 
 char	**ft_matrixdup(char **map)
 {
+	char	**copy;
+	size_t	y;
+	size_t	x;
 
+	y = ft_playlines(map) + 2;
+	x = ft_countboxes(map) + 1;
+	copy = (char *)malloc(sizeof(char *) * y);
+	if (!copy)
+		return (NULL);
+	y = 0;
+	while (map[y] != NULL)
+	{
+		x = 0;
+		copy[y] = (char *)malloc(sizeof(char *) x);
+		if (!copy[y])
+		{
+			while (y > 0)
+				free(copy[--y]);
+			free(copy);
+			return (NULL);
+		}
+		while (map[y][x] != '\0')
+		{
+			copy[y][x] = map[y][x];
+			x++;
+		}
+		copy[y][x] = '\0';
+		y++;
+	}
+	copy[y] = NULL;
+	return (copy);
 }
